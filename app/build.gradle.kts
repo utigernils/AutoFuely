971ae3915adb0870fcca36e579495ac289f01905
utigernils <utigernils@gmail.com>
2026-09-03 10:43:43 +0200

    feat; add github button

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,7 +10,7 @@ android {
         applicationId = "com.utigernils.autofuely"
         minSdk = 24
         targetSdk = 36
-        versionCode = 1
+        versionCode = 3
         versionName = "1.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
